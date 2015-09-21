tv42/becky

commit 18e717cf1c43992f7febee24b0fedd5257564b12

Compute etag at asset generation time
This both makes startup faster and simplifies code. Should have done
it this way in the first place.

fixes https://github.com/tv42/becky/issues/1

asset.go
@@ -21,22 +21,14 @@ import (
 	"time"
 )
 
-var asset_dev = asset(asset.init(asset{Name: "asset_dev.go", Content: "" +
+var asset_dev = asset(asset{Name: "asset_dev.go", Content: "" +
 	"// +build dev\n\npackage main\n\nimport (\n\t\"go/build\"\n\t\"net/http\"\n\t\"os\"\n\t\"path/filepath\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\t// don't bother precomputing ETag if we're reloading from disk\n}\n\nfunc (a asset) init() asset {\n\treturn a\n}\n\nfunc (a asset) importPath() string {\n\t// filled at code gen time\n\treturn \"{{.ImportPath}}\"\n}\n\nfunc (a asset) Open() (*os.File, error) {\n\tpath := a.importPath()\n\tpkg, err := build.Import(path, \".\", build.FindOnly)\n\tif err != nil {\n\t\treturn nil, err\n\t}\n\tp := filepath.Join(pkg.Dir, a.Name)\n\treturn os.Open(p)\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tbody, err := a.Open()\n\tif err != nil {\n\t\t// show the os.Open message, with paths and all, but this only\n\t\t// happens in dev mode.\n\t\thttp.Error(w, err.Error(), http.StatusInternalServerError)\n\t\treturn\n\t}\n\tdefer body.Close()\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
-	""}))
+	"", etag: `"c1Es6vaoVm4="`})
 
 type asset struct {
 	Name    string
 	Content string
 	etag    string
-}
-
-func (a asset) init() asset {
-
-	h := fnv.New64a()
-	_, _ = io.WriteString(h, a.Content)
-	a.etag = `"` + base64.StdEncoding.EncodeToString(h.Sum(nil)) + `"`
-	return a
 }
 
 func (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -47,9 +39,9 @@ func (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	http.ServeContent(w, req, a.Name, time.Time{}, body)
 }
 
-var asset_nodev = asset(asset.init(asset{Name: "asset_nodev.go", Content: "" +
-	"// +build !dev\n\npackage main\n\nimport (\n\t\"encoding/base64\"\n\t\"hash/fnv\"\n\t\"io\"\n\t\"net/http\"\n\t\"strings\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\tetag    string\n}\n\nfunc (a asset) init() asset {\n\t// This is a method to minize the namespace pollution. Use\n\t// chaining to make it callable in variable declarations.\n\th := fnv.New64a()\n\t_, _ = io.WriteString(h, a.Content)\n\ta.etag = `\"` + base64.StdEncoding.EncodeToString(h.Sum(nil)) + `\"`\n\treturn a\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tif a.etag != \"\" && w.Header().Get(\"ETag\") == \"\" {\n\t\tw.Header().Set(\"ETag\", a.etag)\n\t}\n\tbody := strings.NewReader(a.Content)\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
-	""}))
+var asset_nodev = asset(asset{Name: "asset_nodev.go", Content: "" +
+	"// +build !dev\n\npackage main\n\nimport (\n\t\"net/http\"\n\t\"strings\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\tetag    string\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tif a.etag != \"\" && w.Header().Get(\"ETag\") == \"\" {\n\t\tw.Header().Set(\"ETag\", a.etag)\n\t}\n\tbody := strings.NewReader(a.Content)\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
+	"", etag: `"pGCgphv16Ds="`})
 
 var (
 	flagVar  = flag.String("var", "", "variable name to use, \"_\" to ignore (default: file basename without extension)")
@@ -172,7 +164,9 @@ func process(filename, pkg, variable, wrap string) error {
 }
 
 func embed(variable, wrap, filename string, in io.Reader, out io.Writer) error {
-	_, err := fmt.Fprintf(out, "var %s = %s(asset.init(asset{Name: %q, Content: \"\" +\n",
+	h := fnv.New64a()
+	r := io.TeeReader(in, h)
+	_, err := fmt.Fprintf(out, "var %s = %s(asset{Name: %q, Content: \"\" +\n",
 		variable, wrap, filename)
 	if err != nil {
 		return err
@@ -180,7 +174,7 @@ func embed(variable, wrap, filename string, in io.Reader, out io.Writer) error {
 	buf := make([]byte, 1*1024*1024)
 	eof := false
 	for !eof {
-		n, err := in.Read(buf)
+		n, err := r.Read(buf)
 		switch err {
 		case io.EOF:
 			eof = true
@@ -199,7 +193,8 @@ func embed(variable, wrap, filename string, in io.Reader, out io.Writer) error {
 			return err
 		}
 	}
-	if _, err := fmt.Fprintf(out, "\t\"\"}))\n"); err != nil {
+	etag := `"` + base64.StdEncoding.EncodeToString(h.Sum(nil)) + `"`
+	if _, err := fmt.Fprintf(out, "\t\"\", etag: %#q})\n", etag); err != nil {
 		return err
 	}
 	return nil

asset_dev.go.gen.go
@@ -2,6 +2,6 @@
 
 package main
 
-var asset_dev = asset(asset.init(asset{Name: "asset_dev.go", Content: "" +
+var asset_dev = asset(asset{Name: "asset_dev.go", Content: "" +
 	"// +build dev\n\npackage main\n\nimport (\n\t\"go/build\"\n\t\"net/http\"\n\t\"os\"\n\t\"path/filepath\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\t// don't bother precomputing ETag if we're reloading from disk\n}\n\nfunc (a asset) init() asset {\n\treturn a\n}\n\nfunc (a asset) importPath() string {\n\t// filled at code gen time\n\treturn \"{{.ImportPath}}\"\n}\n\nfunc (a asset) Open() (*os.File, error) {\n\tpath := a.importPath()\n\tpkg, err := build.Import(path, \".\", build.FindOnly)\n\tif err != nil {\n\t\treturn nil, err\n\t}\n\tp := filepath.Join(pkg.Dir, a.Name)\n\treturn os.Open(p)\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tbody, err := a.Open()\n\tif err != nil {\n\t\t// show the os.Open message, with paths and all, but this only\n\t\t// happens in dev mode.\n\t\thttp.Error(w, err.Error(), http.StatusInternalServerError)\n\t\treturn\n\t}\n\tdefer body.Close()\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
-	""}))
+	"", etag: `"c1Es6vaoVm4="`})

asset_nodev.go
@@ -3,9 +3,6 @@
 package main
 
 import (
-	"encoding/base64"
-	"hash/fnv"
-	"io"
 	"net/http"
 	"strings"
 	"time"
@@ -15,15 +12,6 @@ type asset struct {
 	Name    string
 	Content string
 	etag    string
-}
-
-func (a asset) init() asset {
-	// This is a method to minize the namespace pollution. Use
-	// chaining to make it callable in variable declarations.
-	h := fnv.New64a()
-	_, _ = io.WriteString(h, a.Content)
-	a.etag = `"` + base64.StdEncoding.EncodeToString(h.Sum(nil)) + `"`
-	return a
 }
 
 func (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {

asset_nodev.go.gen.go
@@ -2,6 +2,6 @@
 
 package main
 
-var asset_nodev = asset(asset.init(asset{Name: "asset_nodev.go", Content: "" +
-	"// +build !dev\n\npackage main\n\nimport (\n\t\"encoding/base64\"\n\t\"hash/fnv\"\n\t\"io\"\n\t\"net/http\"\n\t\"strings\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\tetag    string\n}\n\nfunc (a asset) init() asset {\n\t// This is a method to minize the namespace pollution. Use\n\t// chaining to make it callable in variable declarations.\n\th := fnv.New64a()\n\t_, _ = io.WriteString(h, a.Content)\n\ta.etag = `\"` + base64.StdEncoding.EncodeToString(h.Sum(nil)) + `\"`\n\treturn a\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tif a.etag != \"\" && w.Header().Get(\"ETag\") == \"\" {\n\t\tw.Header().Set(\"ETag\", a.etag)\n\t}\n\tbody := strings.NewReader(a.Content)\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
-	""}))
+var asset_nodev = asset(asset{Name: "asset_nodev.go", Content: "" +
+	"// +build !dev\n\npackage main\n\nimport (\n\t\"net/http\"\n\t\"strings\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\tetag    string\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tif a.etag != \"\" && w.Header().Get(\"ETag\") == \"\" {\n\t\tw.Header().Set(\"ETag\", a.etag)\n\t}\n\tbody := strings.NewReader(a.Content)\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
+	"", etag: `"pGCgphv16Ds="`})

becky.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"go/build"
+	"hash/fnv"
 	"io"
 	"io/ioutil"
 	"log"
@@ -147,7 +149,9 @@ func process(filename, pkg, variable, wrap string) error {
 }
 
 func embed(variable, wrap, filename string, in io.Reader, out io.Writer) error {
-	_, err := fmt.Fprintf(out, "var %s = %s(asset.init(asset{Name: %q, Content: \"\" +\n",
+	h := fnv.New64a()
+	r := io.TeeReader(in, h)
+	_, err := fmt.Fprintf(out, "var %s = %s(asset{Name: %q, Content: \"\" +\n",
 		variable, wrap, filename)
 	if err != nil {
 		return err
@@ -155,7 +159,7 @@ func embed(variable, wrap, filename string, in io.Reader, out io.Writer) error {
 	buf := make([]byte, 1*1024*1024)
 	eof := false
 	for !eof {
-		n, err := in.Read(buf)
+		n, err := r.Read(buf)
 		switch err {
 		case io.EOF:
 			eof = true
@@ -174,7 +178,8 @@ func embed(variable, wrap, filename string, in io.Reader, out io.Writer) error {
 			return err
 		}
 	}
-	if _, err := fmt.Fprintf(out, "\t\"\"}))\n"); err != nil {
+	etag := `"` + base64.StdEncoding.EncodeToString(h.Sum(nil)) + `"`
+	if _, err := fmt.Fprintf(out, "\t\"\", etag: %#q})\n", etag); err != nil {
 		return err
 	}
 	return nil

commit 492ded2544c83ca785f098d3c9dec8c0456d42a2

Add etag field to dev mode asset type, fixes -tags=dev
This was broken in 18e717cf1c43992f7febee24b0fedd5257564b12 because we
now precompute etags, and thus the field is assigned to in the
generated code, instead of the `init` method.

Improve test coverage.

fixes https://github.com/tv42/becky/issues/4

asset.go
@@ -22,8 +22,8 @@ import (
 )
 
 var assetDev = asset(asset{Name: "asset_dev.go", Content: "" +
-	"// +build dev\n\npackage main\n\nimport (\n\t\"go/build\"\n\t\"net/http\"\n\t\"os\"\n\t\"path/filepath\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\t// don't bother precomputing ETag if we're reloading from disk\n}\n\nfunc (a asset) init() asset {\n\treturn a\n}\n\nfunc (a asset) importPath() string {\n\t// filled at code gen time\n\treturn \"{{.ImportPath}}\"\n}\n\nfunc (a asset) Open() (*os.File, error) {\n\tpath := a.importPath()\n\tpkg, err := build.Import(path, \".\", build.FindOnly)\n\tif err != nil {\n\t\treturn nil, err\n\t}\n\tp := filepath.Join(pkg.Dir, a.Name)\n\treturn os.Open(p)\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tbody, err := a.Open()\n\tif err != nil {\n\t\t// show the os.Open message, with paths and all, but this only\n\t\t// happens in dev mode.\n\t\thttp.Error(w, err.Error(), http.StatusInternalServerError)\n\t\treturn\n\t}\n\tdefer body.Close()\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
-	"", etag: `"c1Es6vaoVm4="`})
+	"// +build dev\n\npackage main\n\nimport (\n\t\"go/build\"\n\t\"net/http\"\n\t\"os\"\n\t\"path/filepath\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\tetag    string\n}\n\nfunc (a asset) init() asset {\n\treturn a\n}\n\nfunc (a asset) importPath() string {\n\t// filled at code gen time\n\treturn \"{{.ImportPath}}\"\n}\n\nfunc (a asset) Open() (*os.File, error) {\n\tpath := a.importPath()\n\tpkg, err := build.Import(path, \".\", build.FindOnly)\n\tif err != nil {\n\t\treturn nil, err\n\t}\n\tp := filepath.Join(pkg.Dir, a.Name)\n\treturn os.Open(p)\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tbody, err := a.Open()\n\tif err != nil {\n\t\t// show the os.Open message, with paths and all, but this only\n\t\t// happens in dev mode.\n\t\thttp.Error(w, err.Error(), http.StatusInternalServerError)\n\t\treturn\n\t}\n\tdefer body.Close()\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
+	"", etag: `"Z+My+Q7Ctfk="`})
 
 type asset struct {
 	Name    string

asset_dev.go
@@ -13,7 +13,7 @@ import (
 type asset struct {
 	Name    string
 	Content string
-	// don't bother precomputing ETag if we're reloading from disk
+	etag    string
 }
 
 func (a asset) init() asset {

asset_dev.go.gen.go
@@ -3,5 +3,5 @@
 package main
 
 var assetDev = asset(asset{Name: "asset_dev.go", Content: "" +
-	"// +build dev\n\npackage main\n\nimport (\n\t\"go/build\"\n\t\"net/http\"\n\t\"os\"\n\t\"path/filepath\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\t// don't bother precomputing ETag if we're reloading from disk\n}\n\nfunc (a asset) init() asset {\n\treturn a\n}\n\nfunc (a asset) importPath() string {\n\t// filled at code gen time\n\treturn \"{{.ImportPath}}\"\n}\n\nfunc (a asset) Open() (*os.File, error) {\n\tpath := a.importPath()\n\tpkg, err := build.Import(path, \".\", build.FindOnly)\n\tif err != nil {\n\t\treturn nil, err\n\t}\n\tp := filepath.Join(pkg.Dir, a.Name)\n\treturn os.Open(p)\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tbody, err := a.Open()\n\tif err != nil {\n\t\t// show the os.Open message, with paths and all, but this only\n\t\t// happens in dev mode.\n\t\thttp.Error(w, err.Error(), http.StatusInternalServerError)\n\t\treturn\n\t}\n\tdefer body.Close()\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
-	"", etag: `"c1Es6vaoVm4="`})
+	"// +build dev\n\npackage main\n\nimport (\n\t\"go/build\"\n\t\"net/http\"\n\t\"os\"\n\t\"path/filepath\"\n\t\"time\"\n)\n\ntype asset struct {\n\tName    string\n\tContent string\n\tetag    string\n}\n\nfunc (a asset) init() asset {\n\treturn a\n}\n\nfunc (a asset) importPath() string {\n\t// filled at code gen time\n\treturn \"{{.ImportPath}}\"\n}\n\nfunc (a asset) Open() (*os.File, error) {\n\tpath := a.importPath()\n\tpkg, err := build.Import(path, \".\", build.FindOnly)\n\tif err != nil {\n\t\treturn nil, err\n\t}\n\tp := filepath.Join(pkg.Dir, a.Name)\n\treturn os.Open(p)\n}\n\nfunc (a asset) ServeHTTP(w http.ResponseWriter, req *http.Request) {\n\tbody, err := a.Open()\n\tif err != nil {\n\t\t// show the os.Open message, with paths and all, but this only\n\t\t// happens in dev mode.\n\t\thttp.Error(w, err.Error(), http.StatusInternalServerError)\n\t\treturn\n\t}\n\tdefer body.Close()\n\thttp.ServeContent(w, req, a.Name, time.Time{}, body)\n}\n" +
+	"", etag: `"Z+My+Q7Ctfk="`})

cram/fakehttp.go
@@ -1,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+)
+
+//go:generate -command asset go run asset.go
+//go:generate asset greeting.txt
+
+func txt(a asset) asset {
+	return a
+}
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("fakehttp.go: ")
+
+	rec := httptest.NewRecorder()
+	rec.Body = &bytes.Buffer{}
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	greeting.ServeHTTP(rec, req)
+	if rec.Code != 200 {
+		log.Fatalf("bad response: %v: %s", rec.Code, rec.Body.Bytes())
+	}
+	if _, err := os.Stdout.Write(rec.Body.Bytes()); err != nil {
+		log.Fatal(err)
+	}
+}

cram/dev.t
@@ -1,0 +1,14 @@
+  $ export GOPATH="$PWD"
+  $ mkdir -p src/example.com/myproject
+  $ cd src/example.com/myproject
+  $ cp -- "$TESTDIR/fakehttp.go" .
+  $ cp -- "$TESTDIR/../asset.go" .
+  $ echo Hello, world >greeting.txt
+  $ go generate
+  $ go build -tags=dev -o fakehttp
+  $ ./fakehttp
+  Hello, world
+  $ rm greeting.txt
+  $ ./fakehttp
+  fakehttp.go: bad response: 500: open (.*)/src/example.com/myproject/greeting.txt: no such file or directory (re)
+  [1]